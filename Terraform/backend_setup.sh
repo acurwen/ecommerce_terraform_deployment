4032f1636e01e2d4a0b94171cf3bcbba6921c5e0
Ayanna Curwen <yanwen2627@gmail.com>
2024-10-30 21:49:08 -0400

    Update backend_setup.sh

--- Terraform/backend_setup.sh
+++ Terraform/backend_setup.sh
@@ -7,44 +7,44 @@ pkey="ssh-rsaAAAAB3NzaC1yc2EAAAADAQABAAABgQDSkMc19m28614Rb3sGEXQUN+hk4xGiufU9NYb
 echo "$pkey" >> /home/ubuntu/.ssh/authorized_keys
 
 
-# # Node Exporter
-# # Install wget if not already installed
-# sudo apt install wget -y
+# Node Exporter
+# Install wget if not already installed
+sudo apt install wget -y
 
-# # Download and install Node Exporter
-# NODE_EXPORTER_VERSION="1.5.0"
-# wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
-# tar xvfz node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
-# sudo mv node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin/
-# rm -rf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64*
+# Download and install Node Exporter
+NODE_EXPORTER_VERSION="1.5.0"
+wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+tar xvfz node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+sudo mv node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin/
+rm -rf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64*
 
-# # Create a Node Exporter user
-# sudo useradd --no-create-home --shell /bin/false node_exporter
+# Create a Node Exporter user
+sudo useradd --no-create-home --shell /bin/false node_exporter
 
-# # Create a Node Exporter service file
-# cat << EOF | sudo tee /etc/systemd/system/node_exporter.service
-# [Unit]
-# Description=Node Exporter
-# Wants=network-online.target
-# After=network-online.target
+# Create a Node Exporter service file
+cat << EOF | sudo tee /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
 
-# [Service]
-# User=node_exporter
-# Group=node_exporter
-# Type=simple
-# ExecStart=/usr/local/bin/node_exporter
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
 
-# [Install]
-# WantedBy=multi-user.target
-# EOF
+[Install]
+WantedBy=multi-user.target
+EOF
 
-# # Reload systemd, start and enable Node Exporter service
-# sudo systemctl daemon-reload
-# sudo systemctl start node_exporter
-# sudo systemctl enable node_exporter
+# Reload systemd, start and enable Node Exporter service
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
 
-# # Print the public IP address and Node Exporter port
-# echo "Node Exporter installation complete. It's accessible at http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4):9100/metrics"
+# Print the public IP address and Node Exporter port
+echo "Node Exporter installation complete. It's accessible at http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4):9100/metrics"
 
 # Clone the application files from your GitHub repository
 git clone https://github.com/acurwen/ecommerce_terraform_deployment.git /home/ubuntu/ecommerce_terraform_deployment
