896e401508911ecbcc1f58b261ee966d9ff45a75
Ayanna Curwen <yanwen2627@gmail.com>
2024-10-30 22:35:03 -0400

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,11 +1,11 @@
 #declare variables 
-variable access_key{
+variable aws_access_key{
     type=string
     sensitive=true #meaning its value will be hidden in Terraform's output, logs, and state files
 
 }  #we can say type=string in the curly braces to define variable type 
 #the things we put in the curly brackets are optional
-variable secret_key{
+variable aws_secret_key{
     sensitive=true
 }        # Replace with your AWS secret access key (leave empty if using IAM roles or env vars)
 variable region{
